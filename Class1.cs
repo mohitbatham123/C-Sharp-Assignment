ec1e9411945240f0a4dc7395b30293835f0488cf
mohitbatham123 <99643036+mohitbatham123@users.noreply.github.com>
2023-03-27 22:51:35 +0530

    Update Class1.cs
    using case 1 employee present or not

--- Class1.cs
+++ Class1.cs
@@ -1,27 +1,27 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace EmoyeeManagement
-//{
-//    internal class Class1
-//    {
-//        public static void Presentandabsent()
-//        {
-//            Console.WriteLine("Check employee is present or not");
-//            int employee = 1;                                  // define the variable
-//            Random random = new Random();                      // Random they give the number randomly
-//            int checkemployee = random.Next(0, 2);
-//            if (employee == checkemployee)                   // we apply the ifelse statement condtion true then print the Employee is present 
-//            {                                                // condition is false then print the Employee is absent
-//                Console.WriteLine("Employee is present");
-//            }
-//            else
-//            {
-//                Console.WriteLine("Employee is absent");
-//            }
-//        }   
-//    }
-//}
+namespace EmoyeeManagement
+{
+    internal class Class1
+    {
+       public static void Presentandabsent()
+       {
+           Console.WriteLine("Check employee is present or not");
+           int employee = 1;                                  // define the variable
+           Random random = new Random();                      // Random they give the number randomly
+            int checkemployee = random.Next(0, 2);
+            if (employee == checkemployee)                   // we apply the ifelse statement condtion true then print the Employee is present 
+            {                                                // condition is false then print the Employee is absent
+               Console.WriteLine("Employee is present");
+           }
+           else
+            {
+                Console.WriteLine("Employee is absent");
+           }
+       }   
+    }
+}
