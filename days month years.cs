801c0a37c1bd66ea065f63ce353fdbb16048452a
mohitbatham123 <99643036+mohitbatham123@users.noreply.github.com>
2023-04-12 01:27:50 -0700

    assigment
    Assignment 6

---
+++ days month years.cs
@@ -0,0 +1,39 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace first_riview_practice
+//{
+//    internal class days_month_years
+//    {
+//        public static void Main(string[] args)
+//        {
+//            Console.Write("Please Enter month : ");
+//            int m = Convert.ToInt32(Console.ReadLine());
+//            Console.Write("Please Enter day : ");
+//            int d = Convert.ToInt32(Console.ReadLine());
+//            Console.Write("Please Enter year : ");
+//            int y = Convert.ToInt32(Console.ReadLine());
+
+            
+//            int y0 = y - (14 - m) / 12;
+//            Console.WriteLine("y0 : " + y0);
+
+            
+//            int x = y0 + (y0 / 4) - (y0 / 100) + (y0 / 400);
+//            Console.WriteLine("x : " + x);
+//            int m0 = m + 12 * ((14 - m) / 12) - 2;
+//            Console.WriteLine("m0 : " + m0);
+//            int d0 = (d + x + 31 * m0 / 12) % 2;
+//            Console.WriteLine("d0 : " + d0);
+
+//            string[] days = { "Sunday", "Monday", " Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+
+        
+//            Console.WriteLine("Day of the Week : " + days[d0]);
+
+//        }
+//    }
+//}
---
+++ monthly payment income.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_riview_practice
+{
+    internal class monthly_payment_income
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter principle loan amount");
+            int p = int.Parse(Console.ReadLine());
+            Console.WriteLine("years to pay off ");
+            int Y = int.Parse(Console.ReadLine());
+            Console.WriteLine("percent\r\ninterest compounded monthly");
+            int R = int.Parse(Console.ReadLine());
+
+            int n = 12 * Y;
+            double r = R / (12 * 100);
+
+            double payement = ((p * R) / (1 - Math.Pow((1 + R), -n)));
+            Console.WriteLine(payement);
+
+        }
+    }
+}
